e3826ec5d5575707168d04dabb6edabcad4ce360
dzhxo <dzhao0@stanford.edu>
2019-03-17 21:45:09 -0700

    chg to negate

--- pytorch_pretrained_bert/dcr.py
+++ pytorch_pretrained_bert/dcr.py
@@ -62,12 +62,14 @@ class DCR(torch.nn.Module):
 			max_logit = torch.max(start_logits_ex)
 			#print("STD:")
 			#print(std)
-			if max_logit < (start_logits_ex.std() + start_logits_ex.mean()) or max_logit < (end_logits_ex.std() + end_logits_ex.mean()):
-				print('smaller')
 			#print("MAX LOGIT:")
 			#print(max_logit)
 			# if max logit value is under thresh (i.e. .75), then set 0th index to 1 (max val.) for both start and end logits to signify null.
-			if float(max_logit) < self.null_cosine_thresh:
+			
+			if max_logit < (start_logits_ex.std() + start_logits_ex.mean()) or max_logit < (end_logits_ex.std() + end_logits_ex.mean()):
+				print('smaller')
+
+			# if float(max_logit) < self.null_cosine_thresh:
 				# start_logits_ex[0] = 1.0
 				# end_logits_ex[0] = 1.0
 				start_logits_ex *= -1
